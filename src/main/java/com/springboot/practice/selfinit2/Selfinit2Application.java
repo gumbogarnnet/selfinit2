e54f5eb231f637f48f93632fee4ee49708068d43
Your Name <you@example.com>
2019-03-20 01:57:58 -0700

    first commit

---
+++ src/main/java/com/springboot/practice/selfinit2/Selfinit2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.practice.selfinit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selfinit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selfinit2Application.class, args);
+	}
+
+}
